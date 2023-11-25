59e589de7dc61ae050aa62bec3a95b02b0bf02f4
SNGTech <50254849+SNGTech@users.noreply.github.com>
2023-11-25 14:41:59 +0800

    Landing page: Partial section 3.  Partial carousel card. Animated navbar on scroll

---
+++ RandBox/Client/wwwroot/scripts/navbarFunctions.js
@@ -0,0 +1,16 @@
+﻿window.changeOnScroll = () => {
+    const navbar = document.querySelector('.navbar');
+    const page = document.querySelector('.page');
+    console.log(page);
+    const scrollWatcher = document.createElement('div');
+
+    scrollWatcher.setAttribute('data-scroll-watcher', '');
+    navbar.before(scrollWatcher);
+
+
+    const navObserver = new IntersectionObserver((entries) => {
+        navbar.classList.toggle('scrolled', !entries[0].isIntersecting);
+    });
+
+    navObserver.observe(scrollWatcher);
+};
