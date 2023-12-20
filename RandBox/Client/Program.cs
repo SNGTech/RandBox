781918e13757d7587c0926ad5a04e57be3a725b3
SNGTech <50254849+SNGTech@users.noreply.github.com>
2023-12-20 23:49:20 +0800

    Updated to use ProductService for Landing Page Products Section

--- RandBox/Client/Program.cs
+++ RandBox/Client/Program.cs
@@ -10,15 +10,18 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Http Client that needs Authentication (private API endpoints)
 builder.Services.AddHttpClient("RandBox.ServerAPI.private", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
+// Http Client which can be used Anonymously (public API endpoints)
 builder.Services.AddHttpClient("RandBox.ServerAPI.public", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("RandBox.ServerAPI"));
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("RandBox.ServerAPI.private"));
 
 builder.Services.AddScoped<IGenericService<Category>, CategoryService>();
+builder.Services.AddScoped<IGenericService<Product>, ProductService>();
 
 builder.Services.AddApiAuthorization();
 
---
+++ RandBox/Client/Services/ProductService.cs
@@ -0,0 +1,75 @@
+﻿using RandBox.Client.Services.Contracts;
+using RandBox.Shared.Domain;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace RandBox.Client.Services
+{
+	public class ProductService : IGenericService<Product>
+	{
+		private readonly HttpClient _httpClient_Public;
+		private readonly HttpClient _httpClient_Private;
+
+		public ProductService(IHttpClientFactory clientFactory)
+        {
+			_httpClient_Private = clientFactory.CreateClient("RandBox.ServerAPI.private");
+			_httpClient_Public = clientFactory.CreateClient("RandBox.ServerAPI.public");
+		}
+
+        public async Task<Product> DeleteById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<Product>> DeleteRange(IEnumerable<Product> entities)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<List<Product>> GetAll()
+		{
+			try
+			{
+				var response = await _httpClient_Public.GetAsync("api/Product");
+
+				if (response.IsSuccessStatusCode)
+				{
+					if(response.StatusCode == HttpStatusCode.NoContent)
+					{
+						return Enumerable.Empty<Product>().ToList();
+					}
+					return await response.Content.ReadFromJsonAsync<List<Product>>();
+				}
+				else
+				{
+					var message = await response.Content.ReadAsStringAsync();
+					throw new Exception($"HTTP Status : {response.StatusCode} - {message}");
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+
+		public async Task<Product> GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<Product> Insert(Product entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<Product>> InsertRange(IEnumerable<Product> entities)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<Product> Update(Product entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
