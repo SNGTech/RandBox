72bd17fde521c240099cdc8b22f2dbebce860ea7
PATRICK WU KIN FUNG 2200688F <2200688F@student.tp.edu.sg>
2024-01-22 12:02:31 +0800

    fix revert

---
+++ RandBox/Client/Services/Contracts/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using RandBox.Shared.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RandBox.Client.Services.Contracts
+{
+    public interface IOrderService : IGenericService<Orders>
+    {
+        Task<List<Orders>> GetOrdersByStaffId(int staffId);
+    }
+}
